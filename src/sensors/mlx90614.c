567e924b398e35a38ad5addbe7db669b5a07ab2c
Jason Walter <jaason@protonmail.ch>
2022-03-08 22:00:18 -0500

    TEMP SENSOR CRACKED

--- src/sensors/mlx90614.c
+++ src/sensors/mlx90614.c
@@ -4,6 +4,10 @@
 int mlx90614(float *ptr, mraa_context context)
 {
 	uint8_t buffer[3];
+
+	/* adjust frequency to talk to sensor */
+	mraa_i2c_frequency(i2c, MRAA_I2C_STD);
+
 	/* set I2C bus address */
 	if (mraa_i2c_address(i2c, MLX90614_ADDR) != MRAA_SUCCESS) {
 		return -1;
@@ -15,7 +19,7 @@ int mlx90614(float *ptr, mraa_context context)
 	}
 
 	/* transform data according to datasheet */
-	uint16_t data = (buffer[0] | ((uint16_t)buffer[1] << 8));
+	uint16_t data = (uint16_t)(buffer[0] | ((uint16_t)buffer[1] << 8));
 	*ptr = data;
 
 	if (*ptr == 0) {
@@ -26,5 +30,5 @@ int mlx90614(float *ptr, mraa_context context)
 	*ptr *= 0.2;
 	*ptr -= 273.15;
 
-	return;
+	return 0;
 }
