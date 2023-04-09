24108a08e0b622e2cdab4789b2bd746b14a8c27c
Asher Anand <asherganand@gmail.com>
2023-04-09 15:07:45 -0400

    fixed typedef struct naming convention

--- src/include/threading.h
+++ src/include/threading.h
@@ -4,17 +4,21 @@
 #include <pthread.h>
 #include "include/blast_data.h"
 
-typedef struct {
+typedef struct race_capture {
     /* data */
+    // AGA: we can get away with one big lock
+    sensor *rc_sensors;
+    int num_rc_sensors;
+    pthread_mutex_t rc_data_lock;
 } race_capture;
 
-typedef struct {
+typedef struct car_model {
     race_capture *rc;
     sensor **sensor_key;
     int num_sensors;
 } car_model;
 
-typedef struct {
+typedef struct args {
     car_model *cm;
     pthread_mutex_t *lock_array;
 } args;
