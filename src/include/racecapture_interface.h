040cd4cb2b23da700e0403efb8a3c13d227c0a89
Anjali Kumar <anjkum04@gmail.com>
2023-06-07 11:46:14 -0400

    added infinite loop to rc_serial_read_loop() function and enum struct to racecapture_interface.h. pending: implementing enum infrastructure in rc_serial_read_loop()

--- src/include/racecapture_interface.h
+++ src/include/racecapture_interface.h
@@ -6,6 +6,15 @@
 #define START_FLAG 0xF0
 #define END_FLAG 0XC5
 
+typedef enum {
+    READY;
+    GET_START_FLAG;
+    GET_NUM;
+    GET_SENSOR;
+    GET_END_FLAG;
+    UNSUCCESSFUL_READ
+} states;
+
 int rc_serial_init(void);
 void rc_serial_read_loop(race_capture *rc_data);
 
--- src/racecapture_interface.c
+++ src/racecapture_interface.c
@@ -1,6 +1,7 @@
 #include <mraa.h>
 #include "include/threading.h" // for car model
 #include "include/racecapture_sitl.h"
+#include "include/racecapture_interface.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -28,54 +29,57 @@ int rc_serial_init(void) {
  * @author AGA
  */
 void rc_serial_read_loop(race_capture *rc_data) {
-     char *start_flag_buf= malloc(1);
-     if (mraa_uart_read(uart_contex, start_flag_buf, 1) && *start_flag_buf == START_FlAG) {
-        /* read number of sensors */
-        char *num_sensors_buf= realloc(start_flag_buf, 1);
-        if (mraa_uart_read(uart_contex, num_sensors_buf, 1) && *num_sensors_buf <= 15) {
-            int num_sensors= *num_sensors_buf;
-            char *sensor_buf= realloc(num_sensors_buf, 4);
-            char *tot_sensor_data= malloc(4 * num_sensors);
-            int read_unsuccessful= 0;
-            int i= 0;
+    for (;;) {
+        char *start_flag_buf= malloc(1);
+        if (mraa_uart_read(uart_contex, start_flag_buf, 1) && *start_flag_buf == START_FlAG) {
+            /* read number of sensors */
+            char *num_sensors_buf= realloc(start_flag_buf, 1);
+            if (mraa_uart_read(uart_contex, num_sensors_buf, 1) && *num_sensors_buf <= 15) {
+                int num_sensors= *num_sensors_buf;
+                char *sensor_buf= realloc(num_sensors_buf, 4);
+                char *tot_sensor_data= malloc(4 * num_sensors);
+                int read_unsuccessful= 0;
+                int i= 0;
 
-            while (i < num_sensors && !read_unsuccessful) {
-                if (mraa_uart_read(uart_contex, sensor_buf, 4) == 4) {
-                    strncpy(tot_sensor_data + (i*4), sensor_buf, 4);
-                    sensor_buf= realloc(sensor_buf, 4);
-                    i++;
+                while (i < num_sensors && !read_unsuccessful) {
+                    if (mraa_uart_read(uart_contex, sensor_buf, 4) == 4) {
+                        strncpy(tot_sensor_data + (i*4), sensor_buf, 4);
+                        sensor_buf= realloc(sensor_buf, 4);
+                        i++;
+                    }
+                    else {
+                        read_unsuccessful= 1;
+                    }
+
+                }
+                if (read_unsuccessful) {
+                    printf("Unsuccessful read of sensor %d of %d sensors.\n", i, num_sensors);
+                    free(sensor_buf);
+                    free(tot_sensor_data);
                 }
                 else {
-                    read_unsuccessful= 1;
+                    char *end_flag_buf= realloc(sensor_buf, 1);         /* may cause memory leaks, haven't used realloc too much */
+                    if (mraa_uart_read(uart_contex, end_flag_buf, 1) && *end_flag_buf == END_FLAG) {
+                        /* thread-safe write of data in tot_sensor_data buffer */
+                    }
+                    else {
+                        printf("No expected end flag.\n")
+                        free(tot_sensor_data);
+                    }
+                    free(end_flag_buf);
                 }
-
-            }
-            if (read_unsuccessful) {
-                printf("Unsuccessful read of sensor %d of %d sensors.\n", i, num_sensors);
-                free(sensor_buf);
-                free(tot_sensor_data);
             }
             else {
-                char *end_flag_buf= realloc(sensor_buf, 1);         /* may cause memory leaks, haven't used realloc too much */
-                if (mraa_uart_read(uart_contex, end_flag_buf, 1) && *end_flag_buf == END_FLAG) {
-                    /* thread-safe write of data in tot_sensor_data buffer */
-                }
-                else {
-                    printf("No expected end flag.\n")
-                }
-                free(end_flag_buf);
+                printf("No expected number of sensors.\n");
+                free(num_sensors_buf);
             }
         }
-        else {
-            printf("No expected number of sensors.\n");
-            free(num_sensors_buf);
-        }
-     }
 
-     else {
-        printf("No expected start flag.\n");
-        free(start_flag_buf);
-     }
+        else {
+            printf("No expected start flag.\n");
+            free(start_flag_buf);
+        } 
+    }
 }
 
 static uint8_t read_byte(void) {
